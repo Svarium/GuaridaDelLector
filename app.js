d6c2eb45f1ec8e1fb81bd27e43baa841abaee595
Marcos <marcosbrqz87@gmail.com>
2022-12-29 09:43:36 -0300

    agregamos rutas y html

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.static(path.join(__dirname,  'public' )))
 
 //Rutas
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')))
+app.get('/detalle',(req,res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')))
+app.get('/home',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
+app.get('/registro',(req,res) => res.sendFile(path.join(__dirname, 'views', 'registro.html')))
+app.get('/categoria',(req,res) => res.sendFile(path.join(__dirname, 'views', 'categoria.html')))
+app.get('*',(req,res) => res.sendFile(path.join(__dirname, 'views', '404.html')))
 
 //Servidor levantado
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port))
