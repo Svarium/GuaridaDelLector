cd37f5cc7baa26c9d5f09facf951a42094858353
Svarium <you@example.com>
2023-03-14 17:58:19 -0300

    arreglo middleware de ruta para impedir acceso a usuarios

--- src/middlewares/CheckUser.js
+++ src/middlewares/CheckUser.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
     if(!req.session.userLogin){
-        next()
+        return res.render('login')
     }
 
-    return res.redirect('/')
+  next()
 } 
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { cart } = require('../controllers/cartController');
+const CheckUser = require('../middlewares/CheckUser');
 const router = express.Router();
 
+
 /* /cart */
-router.get('/cart', cart);
-router.get('/cart/:id', cart);
+router.get('/cart', CheckUser ,  cart);
+router.get('/cart/:id', CheckUser, cart);
 
 module.exports = router;
