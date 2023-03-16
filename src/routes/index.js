017665fc35ecda63f7ad8eb632df1881c370899f
John Doe <svariumfoo@gmail.com>
2023-03-16 19:19:51 -0300

    implemento la vista nosotros

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { index, listar, search,admin} = require('../controllers/indexController');
+const { index, listar, search,admin, nosotros} = require('../controllers/indexController');
 const checkUserLogin = require('../middlewares/checkUserLogin');
 var router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', index);
 router.get('/admin',checkUserLogin ,admin)
 router.get('/libros', listar)
 router.get('/search', search)
+router.get('/nosotros', nosotros)
 
 module.exports = router;
