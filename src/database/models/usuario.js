ad234f88878bb05674f8f37e1d4203668c99c886
joey-00 <joseacevedo410@gmail.com>
2023-04-12 11:14:57 -0300

    validacion de registro finalizado

--- src/database/models/usuario.js
+++ src/database/models/usuario.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     surname: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    pass: DataTypes.STRING,
     icon: DataTypes.STRING,
     rolId: DataTypes.INTEGER
   }, {
