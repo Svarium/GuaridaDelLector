647d7968e556b26efc0f6a53814bb282eeae8f7a
John Doe <svariumfoo@gmail.com>
2023-04-12 17:56:25 -0300

    refactor metodo cart para agregar un libro al carrito

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 const path = require('path');
+const db = require("../database/models")
 
-/* const productsFilePath = path.join(__dirname, '../data/books.json');
-
-const libros = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
 
 
 module.exports = {
     cart : (req, res) => {
-         const productsFilePath = path.join(__dirname, '../data/books.json');
+
+
+        db.Libros.findByPk(req.params.id,{
+            include : ['autor']
+        })
+        .then(libro=>{
+            return res.render('cart',{
+                libro
+            })
+        })
+        .catch(error => console.log(error))
+
+       /*   const productsFilePath = path.join(__dirname, '../data/books.json');
         const libros = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
         let id = +req.params.id
 
@@ -16,6 +26,6 @@ module.exports = {
 
         return res.render('cart',{
             libro
-        })
+        }) */
        
  }}
