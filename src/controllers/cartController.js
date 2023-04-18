66be6d42f28acbb27f85bdb5a0a4697e5c13d34d
sebastianGallardoJ <gallardosebastian868@gmail.com>
2023-04-18 19:58:27 -0300

    agrega sugerencias a la vista carrito ademas la sugerencia del profe sobre el conteo de libros en carrito

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -6,13 +6,21 @@ const db = require("../database/models")
 
 module.exports = {
     cart : (req, res) => {
+       const libros = db.Libros.findAll({
+        order: [["createdAt", "ASC"]],
+        limit: 6,
+        include:["genero"]
 
+        })
 
-        db.Libros.findByPk(req.params.id,{
+     const libro = db.Libros.findByPk(req.params.id,{
             include : ['autor']
         })
-        .then(libro=>{
+
+        Promise.all([libro,libros])
+        .then(([libro,libros])=>{
             return res.render('cart',{
+                libros,
                 libro
             })
         })
